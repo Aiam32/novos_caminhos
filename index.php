d778a73bcb3a980e67b3080b9c1926ab1a09fc84
Ugo Maia Fernandes <fernandes.ugo@gmail.com>
2020-09-13 21:23:54 -0300

    Comandos de saída

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+/* TODO REVISAR CÁLCULO DE AMORTIZAÇÃO */
+/* FIXME CORRIGIR CÁLCULO DAS MÉDIAS DOS ALUNOS */
+
+/* @author Ugo <fernandes.ugo@gmail.com>/*
+phpinfo();
+
+/**
+ * Class constructor.
+ */
+public function __construct(type $ = null)
+{
+    $this-> = $;
+}
+
+if ($var) {
+    
+}
