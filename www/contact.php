2b3426864557b8e324c24f0f1be126cc5e714847
Aiam32 <fernandes.ugo@gamil.com>
2021-01-02 18:14:09 -0300

    Atualizando Web Design

--- www/contact.php
+++ www/contact.php
@@ -112,19 +112,16 @@
                 <aside class="col-12 col-md-12 col-lg-2">
                     <div class="sidebar box sticky-column">
                         <ul class="nav">
-
                             <li class="nav__item">
                                 <a class="active" href="index.html">
                                     <font color="GhostWhite"><i class="icon-user"></i>Sobre Mim</font>
                                 </a>
                             </li>
-                            <br><br>
                             <li class="nav__item">
                                 <a href="resume.html">
                                     <font color="GhostWhite"><i class="icon-file-text"></i>Currículo</font>
                                 </a>
                             </li>
-                            <br><br>
                             <li class="nav__item">
                                 <a href="works.html">
                                     <font color="GhostWhite"><i class="icon-codesandbox"></i>Portfólio</font>
